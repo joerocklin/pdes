ee608bd2edc7e09aaf1e71b89a3d8d599f1e7d1b
Joe Rocklin <joe.rocklin@gmail.com>
2012-08-08 19:04:19 -0400

    Removing some test code after creating a new branch for it

--- test_apps/simternet/simternet.cxx
+++
@@ -1,8 +0,0 @@
-#include <warped/WarpedMain.h>
-#include "simternetApplication.h"
-
-int main(int argc, char** argv) {
-  WarpedMain wm( new SimternetApplication() );
-  
-  return wm.main( argc, argv );
-}
